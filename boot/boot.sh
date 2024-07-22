89bee283443a0d2621c7f542c00c7c9dd802f20b
boyi <1873672486@qq.com>
2024-07-22 23:52:43 +0800

    Virtual Address!

--- boot/boot.sh
+++ boot/boot.sh
@@ -2,7 +2,7 @@ nasm -i include/ -o loader.bin loader.S
 nasm -i include/ -o mbr.bin mbr.S
 
 dd if=mbr.bin of=../bochs/hd60M.img bs=512 count=1  conv=notrunc
-dd if=loader.bin of=../bochs/hd60M.img bs=512 count=1 seek=2 conv=notrunc
+dd if=loader.bin of=../bochs/hd60M.img bs=512 count=3 seek=2 conv=notrunc
 
 cd ../bochs
 bochs -f bochsrc.disk
